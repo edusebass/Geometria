74a112c18671ec663c092d6136ad37b16b6d492c
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-06-06 07:30:06 -0500

    añadi figuras 3D

--- src/figuras2D.java
+++ src/figuras2D.java
@@ -1,2 +1,18 @@
 public class figuras2D {
+    int nlados;
+
+    public figuras2D(int nlados) {
+        this.nlados = nlados;
+    }
+
+    public int getNlados() {
+        return nlados;
+    }
+    public void setNlados(int nlados) {
+        this.nlados = nlados;
+    }
+
+    areaCuadrada() {
+
+    }
 }
--- src/figuras3D.java
+++ src/figuras3D.java
@@ -1,2 +1,4 @@
 public class figuras3D {
+
+
 }
